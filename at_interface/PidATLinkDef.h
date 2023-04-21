472588d8a081c27a1b9c2f5c49c795670a6ac7cc
Oleg Golosov <oleg.golosov@gmail.com>
2023-04-21 14:32:59 +0300

    remove AT stuff

--- at_interface/PidATLinkDef.h
+++
@@ -1,8 +0,0 @@
-#ifndef PID_AT_INTERFACE_PIDATLINKDEF_H_
-#define PID_AT_INTERFACE_PIDATLINKDEF_H_
-
-#pragma link off all globals;
-#pragma link off all classes;
-#pragma link off all functions;
-
-#endif //PID_AT_INTERFACE_PIDATLINKDEF_H_
---
+++ macro/rootlogon.C
@@ -0,0 +1,3 @@
+{
+  gSystem->Load( "../build/src/libPid");
+}
